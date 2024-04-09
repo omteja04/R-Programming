b265d229cb971d129207a0a439149715098da9ad
omteja04 <22P31A0539@acet.ac.in>
2024-04-09 23:16:44 +0530

    Programmed an R script to multiply two vectors of integer type and length 3.

---
+++ LAB/Exercise-19.r
@@ -0,0 +1,15 @@
+# Author: omteja04
+# Created on: 11-03-2024 09:53:23
+# Description: Write a R program to multiply two vectors of integers type and length 3.
+
+# Create two vectors of integers with length 3
+vector1 <- c(2, 4, 6)
+vector2 <- c(3, 5, 7)
+
+# Multiply the two vectors element-wise
+result_vector <- vector1 * vector2
+
+# Print the original vectors and the result
+cat("Vector 1: ", vector1, "\n")
+cat("Vector 2: ", vector2, "\n")
+cat("Result: ", result_vector, "\n")
