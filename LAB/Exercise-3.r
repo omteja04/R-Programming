f4bbee145030f6976ec8d5670231d754927e2028
omteja04 <22P31A0539@acet.ac.in>
2024-04-09 23:11:43 +0530

    Developed an R program to generate a sequence of numbers from 20 to 50, calculating the mean of numbers from 20 to 60 and the sum of numbers from 51 to 91.

---
+++ LAB/Exercise-3.r
@@ -0,0 +1,11 @@
+# Author: omteja04
+# Created on: 11-03-2024 10:00:44
+# Description: Write a R program to create a sequence of numbers from 20 to 50 and find the mean of numbers from 20 to 60 and sum of numbers from 51 to 91.
+
+print("Sequence of numbers from 20 to 50:")
+print(seq(20, 50))
+print("Mean of numbers from 20 to 60:")
+print(mean(20:60))
+print("Sum of numbers from 51 to 91:")
+print(sum(51:91))
+
