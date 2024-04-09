595afb2548615c958c8201e63f313b98126f5a79
omteja04 <22P31A0539@acet.ac.in>
2024-04-09 23:18:51 +0530

    Developed an R program to create a list containing a vector, a matrix, and a list, assigning names to the elements in the list, and accessing the first and second elements of the list.

---
+++ LAB/Exercise-22.r
@@ -0,0 +1,32 @@
+# Author: omteja04
+# Created on: 09-04-2024 22:11:56
+# Description: Write a R program to create a list containing a vector, a matrix and a list and give names to the elements in the list. Access the first and second element of the list
+
+# Create a vector
+vector_data <- c(1, 2, 3, 4, 5)
+
+# Create a matrix
+matrix_data <- matrix(1:9, nrow = 3)
+
+# Create another list
+inner_list <- list(name = "John", age = 30)
+
+# Create a main list containing the vector, matrix, and inner list
+main_list <- list(
+  my_vector = vector_data,
+  my_matrix = matrix_data,
+  my_inner_list = inner_list
+)
+
+# Give names to the elements in the main list
+names(main_list) <- c("Vector", "Matrix", "Inner_List")
+
+# Access the first element of the list
+first_element <- main_list[[1]]
+print("First element of the list:")
+print(first_element)
+
+# Access the second element of the list
+second_element <- main_list[[2]]
+print("Second element of the list:")
+print(second_element)
