2f19ffd004d9afb9afeb72fb719928057765d75d
omteja04 <22P31A0539@acet.ac.in>
2024-04-09 23:12:31 +0530

    Designed an R program to create three vectors and combine them into a 3x3 matrix, where each column represents a vector. Printed the content of the matrix.

---
+++ LAB/Exercise-6.r
@@ -0,0 +1,12 @@
+# Author: omteja04
+# Created on: 09-04-2024 20:45:08
+# Description: Write a R program to create three vectors a,b,c with 3 integers. Combine the three vectors to become a 3×3 matrix where each column represents a vector. Print the content of the matrix.
+
+
+
+a <- c(1, 2, 3)
+b <- c(4, 5, 6)
+c <- c(7, 8, 9)
+m <- cbind(a, b, c)
+print("Content of the said matrix:")
+print(m)
