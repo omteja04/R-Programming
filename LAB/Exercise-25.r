c734d7a3342bdbdffb556e428ec9ad757bdb0dd8
omteja04 <22P31A0539@acet.ac.in>
2024-04-09 23:19:20 +0530

    Implemented an R program to merge two given lists into one list.

---
+++ LAB/Exercise-25.r
@@ -0,0 +1,18 @@
+# Author: omteja04
+# Created on: 11-03-2024 10:07:40
+# Description: Write a R program to merge two given lists into one list.
+
+# Given lists
+list1 <- list(1, 2, 3)
+list2 <- list("Teja", "ACET", "CSE")
+
+# Create a new list containing the two original lists
+merged_list <- list(list1, list2)
+
+# Print the original lists and the merged list
+cat("List 1: ", "\n")
+print(list1)
+cat("List 2: ", "\n")
+print(list2)
+cat("Merged List: ", "\n")
+print(merged_list)
