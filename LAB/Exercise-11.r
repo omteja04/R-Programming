6e91a1610d4077938a6a22c46b907dc12e6a5dc4
omteja04 <22P31A0539@acet.ac.in>
2024-04-09 23:13:18 +0530

    Implemented an R program to create an empty data frame.

---
+++ LAB/Exercise-11.r
@@ -0,0 +1,10 @@
+# Author: omteja04
+# Created on: 09-04-2024 21:48:36
+# Description: Write a R program to create an empty data frame.
+
+# Create an empty data frame
+empty_df <- data.frame()
+
+# Print the empty data frame
+cat("Empty Data Frame:\n")
+print(empty_df)
