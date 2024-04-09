6b8f427eb361f4b4bf3d831f7310e1f09b86c20f
omteja04 <22P31A0539@acet.ac.in>
2024-04-09 23:19:43 +0530

    Crafted an R program to assign new names "a", "b", and "c" to the elements of a given list.

---
+++ LAB/Exercise-27.r
@@ -0,0 +1,12 @@
+# Author: omteja04
+# Created on: 09-04-2024 22:28:14
+# Description: Write a R program to assign new names "a", "b" and "c" to the elements of a given list.
+
+# Create a sample list
+given_list <- list("element1" = 1, "element2" = 2, "element3" = 3)
+
+# Assign new names "a", "b", and "c" to the elements
+names(given_list) <- c("a", "b", "c")
+
+# Print the list with new names assigned
+print(given_list)
