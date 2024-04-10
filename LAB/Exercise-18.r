ddcd4316aef82c9222e34833527cc59184463076
omteja04 <22P31A0539@acet.ac.in>
2024-04-10 08:55:46 +0530

    Crafted an R program to append a value to a given empty vector.

--- LAB/Exercise-18.r
+++ LAB/Exercise-18.r
@@ -15,3 +15,11 @@ empty_vector <- append(empty_vector, 25)
 
 # Print the resulting vector
 print(empty_vector)
+
+# Creating Empty Vectors:
+# In R, an empty vector can be created using the vector() function without any arguments. 
+# This creates a vector with zero length, which can then be populated with values.
+
+# Appending Values:
+# Values can be appended to a vector in R using either the concatenation operator c() or the append() function.
+#  The c() function concatenates vectors, while the append() function appends values to a vector.
