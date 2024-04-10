2efbfafe269bf7c0160ac1d8e6a1655cdb52b1fe
omteja04 <22P31A0539@acet.ac.in>
2024-04-10 09:10:28 +0530

    Created an R program to generate a two-dimensional 5x3 array containing a sequence of even integers greater than 50.

--- LAB/Exercise-9.r
+++ LAB/Exercise-9.r
@@ -8,3 +8,13 @@ even_integers_array <- matrix(seq(52, by = 2, length.out = 5 * 3), nrow = 5,byro
 # Print the array
 cat("5x3 Array of Even Integers Greater than 50:\n")
 print(even_integers_array)
+
+# Generating Sequences of Even Integers:
+# In R, the seq() function is used to generate sequences of numbers. 
+# By specifying appropriate parameters such as the starting value, increment (by), and length,
+#  it's possible to generate sequences of even integers.
+
+# Creating Matrices from Sequences:
+# The matrix() function in R reshapes a vector or a sequence into a matrix with specified dimensions. By specifying the number of rows (nrow) and columns (ncol), 
+# as well as optionally indicating whether to fill the matrix row-wise or column-wise,
+#  we can create matrices of desired shapes.
