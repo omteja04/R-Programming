25b4262b5d8272f7a574b5799c1c4fc7667d76df
omteja04 <22P31A0539@acet.ac.in>
2024-04-10 08:57:40 +0530

    Developed an R program to find the row and column indices of the maximum and minimum values in a given matrix.

--- LAB/Exercise-17.r
+++ LAB/Exercise-17.r
@@ -18,3 +18,12 @@ min_value <- min(sample_matrix)
 min_index <- which(sample_matrix == min_value, arr.ind = TRUE)
 print("Indices of Minimum Value:")
 print(min_index)
+
+# Finding Maximum and Minimum Values:
+# In R, the max() and min() functions are used to find the maximum and minimum values, respectively,
+#  in a vector or matrix. The which() function is then used to determine the indices of these extreme values,
+#   optionally specifying arr.ind = TRUE to return the indices as row-column pairs.
+
+# Row and Column Indices:
+# The which() function, when used with arr.ind = TRUE, returns indices as row-column pairs.
+#  This allows for precise identification of the location of maximum and minimum values within the matrix.
