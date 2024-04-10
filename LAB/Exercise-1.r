31a523a564d902b1798f867adc6473c652d7a99c
omteja04 <22P31A0539@acet.ac.in>
2024-04-10 07:57:30 +0530

    Created an R program to prompt the user for name and age, displaying the input values. Also included printing the version of R installation.

--- LAB/Exercise-1.r
+++ LAB/Exercise-1.r
@@ -6,3 +6,16 @@ name <- readline(prompt = "Input your name: ")
 age <- readline(prompt = "Input your age: ")
 print(paste("My name is", name, "and I am", age, "years old."))
 print(R.version.string)
+
+
+# 1. **User Input:**
+#    - `readline()` function is used to prompt the user to input their name and age.
+#    - The inputs are stored in variables `name` and `age`.
+
+# 2. **Display Input:**
+#    - The `print()` function is used to display the values entered by the user.
+#    - `paste()` function is utilized to concatenate strings for the output message.
+
+# 3. **R Version:**
+#    - `R.version.string` contains information about the current R installation.
+#    - `print()` function is used to display the R version.
