e4749785f59d79cc8068cf289242b0371f788e28
omteja04 <22P31A0539@acet.ac.in>
2024-04-10 08:13:43 +0530

    Programmed an R script to select the second element of a given nested list.

--- LAB/Exercise-24.r
+++ LAB/Exercise-24.r
@@ -15,3 +15,13 @@ second_element <- nested_list[[2]]
 # Print the second element
 print("Second element of the nested list:")
 print(second_element)
+
+
+# Nested Lists in R:
+# Nested lists in R are lists that contain other lists as elements. 
+# They can have multiple levels of nesting, allowing for complex data structures. Each inner list can contain elements of different types, making nested lists versatile for storing heterogeneous data.
+
+# Selecting Elements from Nested Lists:
+# Elements from nested lists can be selected using double square brackets [[ ]]. 
+# The indices within the double brackets specify the position of the desired element within the nested structure. 
+# In this case, nested_list[[2]] selects the second inner list from the nested_list.
