19e9512bddf830b4e90feb246cb5455345bcb600
omteja04 <22P31A0539@acet.ac.in>
2024-04-10 09:07:48 +0530

    Implemented an R program to create an empty data frame.

--- LAB/Exercise-11.r
+++ LAB/Exercise-11.r
@@ -8,3 +8,11 @@ empty_df <- data.frame()
 # Print the empty data frame
 cat("Empty Data Frame:\n")
 print(empty_df)
+
+# Empty Data Frames:
+# An empty data frame in R is a data frame that contains no rows or columns.
+#  It serves as a placeholder for storing data that may be added later or as a starting point for data manipulation tasks.
+
+# Creating Empty Data Frames:
+# An empty data frame can be created using the data.frame() function without specifying any arguments. Alternatively,
+# you can create an empty data frame with specific column names by passing empty vectors or NA values as arguments to the data.frame() function.
