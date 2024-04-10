36b1309934446a1f70206c67756d3347cc37eb68
omteja04 <22P31A0539@acet.ac.in>
2024-04-10 08:09:22 +0530

    Crafted an R program to assign new names "a", "b", and "c" to the elements of a given list.

--- LAB/Exercise-27.r
+++ LAB/Exercise-27.r
@@ -10,3 +10,15 @@ names(given_list) <- c("a", "b", "c")
 
 # Print the list with new names assigned
 print(given_list)
+
+
+
+# Lists in R:
+# Lists are versatile data structures in R that can contain elements of different types, 
+# including vectors, matrices, other lists, functions, etc. Each element in a list can have a name,
+#  which can be used to reference or manipulate the element.
+
+# Assigning Names to List Elements:
+# The names() function in R is used to assign or retrieve the names of elements in a list.
+#  By specifying the names as a character vector and assigning it to names(given_list), 
+#  we can assign new names to the elements of a list.
