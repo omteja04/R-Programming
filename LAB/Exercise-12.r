12be5cf7eb63504386cd6dec6e0c5352a646d622
omteja04 <22P31A0539@acet.ac.in>
2024-04-10 09:06:01 +0530

    Developed an R program to construct a data frame from four given vectors.

--- LAB/Exercise-12.r
+++ LAB/Exercise-12.r
@@ -14,3 +14,13 @@ my_data_frame <- data.frame(Name = name, Age = age, City = city, Grade = grade)
 # Print the data frame
 cat("Data Frame:\n")
 print(my_data_frame)
+
+# Data Frame in R:
+# A data frame is a tabular data structure in R similar to a spreadsheet or a database table. 
+# It consists of rows and columns, where each column can contain different types of data. 
+# Data frames are commonly used for organizing and analyzing structured data.
+
+# Creating Data Frames:
+# The data.frame() function in R is used to create data frames. 
+# It takes vectors as input and combines them into a data frame,
+#  where each vector becomes a column in the data frame.
