8864d8a3dd516be9c01e62562ee5192cca814b49
omteja04 <22P31A0539@acet.ac.in>
2024-04-10 09:02:28 +0530

    Implemented an R program to save the information of a data frame in a file and display the information of the file.

--- LAB/Exercise-14.r
+++ LAB/Exercise-14.r
@@ -18,3 +18,19 @@ read_data <- read.csv("my_data.csv")
 
 cat("Content of the CSV File:\n")
 print(read_data)
+
+
+# Data Frame in R:
+# A data frame is a tabular data structure in R similar to a spreadsheet or a database table.
+# It consists of rows and columns, where each column can contain different types of data.
+# Data frames are commonly used for organizing and analyzing structured data.
+
+# Saving Data Frames to CSV Files:
+# The write.csv() function in R is used to save data frames to CSV (comma-separated values) files.
+# It creates a CSV file containing the data from the data frame.
+#  The row.names parameter specifies whether row names should be included in the CSV file.
+
+# Reading Data from CSV Files:
+# The read.csv() function in R is used to read data from CSV files into a data frame.
+# It reads the content of the CSV file and converts it into a structured data frame,
+#  which can then be manipulated and analyzed in R.
