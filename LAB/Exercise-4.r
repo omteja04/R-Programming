6fb3e3185e06f6f90781c33629af38480d594b4c
omteja04 <22P31A0539@acet.ac.in>
2024-04-10 08:02:36 +0530

    Crafted an R program to generate a simple bar plot of marks for five subjects.

--- LAB/Exercise-4.r
+++ LAB/Exercise-4.r
@@ -11,3 +11,19 @@ barplot(marks,
   col = "darkred",
   horiz = FALSE
 )
+
+# Generating Bar Plot:
+
+# barplot() function is used to create the bar plot.
+# marks is passed as the input vector.
+# main argument specifies the title of the plot as "Comparing marks of 5 subjects".
+# xlab and ylab arguments specify the labels for the x-axis and y-axis, respectively.
+# names.arg argument specifies the names for the bars, corresponding to the subjects.
+# col argument sets the color of the bars to "darkred".
+# horiz argument specifies whether the bars should be horizontal (TRUE) or vertical (FALSE). In this case, it's set to FALSE, indicating vertical bars.
+
+# Bar Plot (barplot()):
+# The barplot() function in R is used to create bar plots.
+# It takes a numeric vector as input and generates bars of corresponding heights.
+# Various arguments can be used to customize the appearance of the bar plot,
+# such as title (main), axis labels (xlab, ylab), names for the bars (names.arg), colors (col), and orientation (horiz).
