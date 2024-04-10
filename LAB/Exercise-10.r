2e89b17fedd1e725f2384a866a52d73d043d809d
omteja04 <22P31A0539@acet.ac.in>
2024-04-10 09:09:21 +0530

    Designed an R script to construct an array using specified columns, rows, and tables, displaying the content of the resulting array.

--- LAB/Exercise-10.r
+++ LAB/Exercise-10.r
@@ -8,3 +8,13 @@ data <- array(1:24, dim = c(3, 4, 2))
 # Print the content of the array
 cat("Array Content:\n")
 print(data)
+
+# Three-Dimensional Arrays in R:
+# A three-dimensional array in R is a data structure that extends the concept of a matrix to three dimensions. 
+# It consists of multiple two-dimensional matrices stacked on top of each other, forming a cube-like structure.
+
+# Creating Arrays:
+# The array() function in R is used to create arrays. 
+# It takes a vector of data elements as input and specifies the dimensions of the array using the dim parameter.
+#  For a three-dimensional array, the dimensions are specified as the number of rows, columns, 
+#  and "depths" (or "layers").
